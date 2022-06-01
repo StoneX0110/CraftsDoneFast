3f2c8c48a441fd0611ddbfc71ed4aff91ba6944b
Daniel Testor <daniel.testor@hotmail.com>
2022-06-01 15:54:48 +0200

    [add] setup backend, fix CORS error, connect db

---
+++ server.js
@@ -0,0 +1,38 @@
+// const express = require('express');
+// const cors = require('cors');
+let express = require('express'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    database = require('./database'),
+    bodyParser = require('body-parser');
+const createError = require('http-errors');
+require('dotenv').config();
+
+// Connect mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(database.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Database connected")
+},
+    error => {
+        console.log("Database could't be connected to: " + error)
+    }
+)
+const app = express();
+const port = process.env.PORT || 5000;
+// parse application/json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+const jobOfferAPI = require('./routes/jobOffer');
+app.use('/api/jobOffer', jobOfferAPI);
+
+
+app.use(cors());
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
