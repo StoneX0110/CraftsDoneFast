e0cbb039a897753e7283d05f9ef0f6cee2fcf528
Patrick Litschel <ge49zel@mytum.de>
2022-06-25 19:37:50 +0200

    [chg] updated path to user profile page - some small refactoring in UserView.js

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const authJwt = require('../authentication/auth');
-const {getUser, updateUser} = require("../controllers/user");
+const {getUser, updateUser, getMatchingProfiles,getMatchingProfilesInRange} = require("../controllers/user.js");
 const verifyToken = authJwt.verifyToken;
 
-router.get('/:username', getUser);
+router.get('/profile/:username', getUser);
+
+router.route('/matchingProfilesInRange').get(getMatchingProfilesInRange);
+
+router.route('/matchingProfiles').get(getMatchingProfiles);
 
 router.route('/update').post([verifyToken], updateUser);
+
+
 
 module.exports = router;
