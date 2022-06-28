52afb76ed33cd4dff2a4a0deab2af075b5277b15
Jannis Bittmayer <ge49foh@mytum.de>
2022-06-28 17:21:15 +0200

    [add] route + function to get all chats of user (without messages & with username of chat partner)

--- controllers/chat.js
+++ controllers/chat.js
@@ -30,3 +30,43 @@ exports.createChat = ((req, res) => {
         )
     }
 )
+
+// returns all chats of a specific user with additional info (username of chat partners)
+exports.getMyChats = ((req, res) => {
+    userModel.findById(req.userId).select(['chats', 'username']).then(function (userInfo) {
+        let chatIds = userInfo.chats.map((chatObjectId) => {
+            return chatObjectId.valueOf()
+        });
+
+        let chatsWithoutMessages = [];
+        //counter to only send when both chats are received
+        let chatCounter = 0;
+        //add each chat without messages to result
+        chatIds.forEach(chatId => {
+            chatModel.findById(chatId).select(['-messages']).then(function (resultChat, err) {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    //add partner username to result
+                    let partnerId = '';
+                    let partnerUsername = '';
+                    //set partner id to id of chat participant that isn't us
+                    if (req.userId.toString() === resultChat.users.craftsman.toString()) {
+                        partnerId = resultChat.users.client;
+                    } else {
+                        partnerId = resultChat.users.craftsman;
+                    }
+                    userModel.findById(partnerId).select(['username']).then(function (username) {
+                        partnerUsername = username.username;
+                        chatsWithoutMessages.push({chat: resultChat, partnerUsername: partnerUsername});
+                        chatCounter++;
+                        //if all chats are included, send result
+                        if (chatCounter === chatIds.length) res.send(chatsWithoutMessages);
+                    })
+
+                }
+            })
+        })
+    })
+});
--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authJwt = require('../authentication/auth');
-const {createChat} = require("../controllers/chat");
+const {createChat, getMyChats} = require("../controllers/chat");
 const verifyToken = authJwt.verifyToken;
 
 router.route('/create').post([verifyToken], createChat);
+
+router.route('/getMyChats').get([verifyToken], getMyChats);
 
 module.exports = router;
