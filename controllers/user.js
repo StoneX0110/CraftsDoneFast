8a69df920ee9f69949aae9465c9cbb6d21e036f4
Florian Stang <ge25jac@mytum.de>
2022-07-02 12:01:32 +0200

    [add] profilePicture update to updateUser

--- controllers/user.js
+++ controllers/user.js
@@ -11,7 +11,10 @@ exports.getUser = ((req, res) => {
 exports.updateUser = ((req, res) => {
     const user = req.body;
     //console.log(user);
-    userModel.findByIdAndUpdate(req.userId, {$set: {"settings": user.state}}).then(function (us, err) {
+    const profilePicture = req.body.profilePicture;
+    const transformedProfilePicture = {data: new Buffer.from(profilePicture, 'base64'), contentType: "image/jpeg"};
+    console.log(transformedProfilePicture);
+    userModel.findByIdAndUpdate(req.userId, {$set: {"settings": user.state, "profilePicture": transformedProfilePicture}}).then(function (us, err) {
         if (err) {
             console.log(err);
             res.send(err);
