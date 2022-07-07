1bbf4f8c88f35d6bf7d0b20d4fae82d2bd3604f1
Jannis Bittmayer <ge49foh@mytum.de>
2022-07-07 17:40:53 +0200

    [add] delete chat function [chg] code formatting Co-developed with Florian Stang.

--- controllers/chat.js
+++ controllers/chat.js
@@ -60,177 +60,215 @@ exports.createChat = ((req, res) => {
 
 
 // returns all chats of a specific user with additional info (username of chat partners)
-    exports.getMyChats = ((req, res) => {
-        userModel.findById(req.userId).select(['chats', 'username']).then(function (userInfo) {
-            let chatIds = userInfo.chats.map((chatObjectId) => {
-                return chatObjectId.valueOf()
-            });
+exports.getMyChats = ((req, res) => {
+    userModel.findById(req.userId).select(['chats', 'username']).then(function (userInfo) {
+        if (userInfo.chats.length === 0) res.send([]);
+        let chatIds = userInfo.chats.map((chatObjectId) => {
+            return chatObjectId.valueOf()
+        });
 
-            let chatsWithMessages = [];
-            //counter to only send when both chats are received
-            let chatCounter = 0;
-            //add each chat to result
-            chatIds.forEach(chatId => {
-                chatModel.findById(chatId).then(async function (resultChat, err) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                    } else {
-                        //add messages to chat
-                        let messages = [];
-                        let msgCounter = 0;
-                        for (const msgId of resultChat.messages) {
-                            let messageId = msgId.valueOf();
-                            await messageModel.findById(messageId).select(['-chat']).then(function (message, err) {
-                                if (err) {
-                                    console.log(err);
-                                    res.send(err);
-                                } else {
-                                    messages.push(message);
-                                    msgCounter++;
-                                    if (msgCounter === resultChat.messages.length) {
-                                        resultChat.messages = messages;
-                                    }
+        let chatsWithMessages = [];
+        //counter to only send when both chats are received
+        let chatCounter = 0;
+        //add each chat to result
+        chatIds.forEach(chatId => {
+            chatModel.findById(chatId).then(async function (resultChat, err) {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    //add messages to chat
+                    let messages = [];
+                    let msgCounter = 0;
+                    for (const msgId of resultChat.messages) {
+                        let messageId = msgId.valueOf();
+                        await messageModel.findById(messageId).select(['-chat']).then(function (message, err) {
+                            if (err) {
+                                console.log(err);
+                                res.send(err);
+                            } else {
+                                messages.push(message);
+                                msgCounter++;
+                                if (msgCounter === resultChat.messages.length) {
+                                    resultChat.messages = messages;
                                 }
-                            })
-                        }
-                        //add partner username to result
-                        let partnerId = '';
-                        let partnerUsername = '';
-                        //set partner id to id of chat participant that isn't us
-                        if (req.userId.toString() === resultChat.users.craftsman.toString()) {
-                            partnerId = resultChat.users.client;
-                        } else {
-                            partnerId = resultChat.users.craftsman;
-                        }
-                        userModel.findById(partnerId).select(['username']).then(function (username) {
-                            partnerUsername = username.username;
-                            chatsWithMessages.push({chat: resultChat, partnerUsername: partnerUsername});
-                            chatCounter++;
-                            //if all chats are included, send result
-                            if (chatCounter === chatIds.length) res.send(chatsWithMessages);
+                            }
                         })
-
                     }
-                })
+                    //add partner username to result
+                    let partnerId = '';
+                    let partnerUsername = '';
+                    //set partner id to id of chat participant that isn't us
+                    if (req.userId.toString() === resultChat.users.craftsman.toString()) {
+                        partnerId = resultChat.users.client;
+                    } else {
+                        partnerId = resultChat.users.craftsman;
+                    }
+                    userModel.findById(partnerId).select(['username']).then(function (username) {
+                        partnerUsername = username.username;
+                        chatsWithMessages.push({chat: resultChat, partnerUsername: partnerUsername});
+                        chatCounter++;
+                        //if all chats are included, send result
+                        if (chatCounter === chatIds.length) res.send(chatsWithMessages);
+                    })
+
+                }
             })
         })
-    });
+    })
+});
 
-    exports.postMessageToChat = ((req, res) => {
-        let message = req.body;
-        messageModel(message).save((err, createdMessage) => {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                chatModel.findByIdAndUpdate(createdMessage.chat, {$push: {"messages": createdMessage._id.valueOf()}}).then(function (us, err) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                    }
-                })
-            }
-        })
-    });
-
-//TODO: aktuell wir immer noch ein neuer Contract erstell selbst wenn schon einer besteht -> der alte wird nur nicht mehr referenziert und exestiert aber noch dieser muss gelöscht werden
-    /* Lösungsansatz 1: wir holen immer den contract und tauschen werte aus oder
-       Lösungsansatz 2: wir löschen den alten
-       Lösungsansatz 3: wir checken ob schon ein contract besteht und holen uns dann den alten contract direkt im popup und updaten diesen
-     */
-    exports.createContract = ((req, res) => {
-        const contract = req.body;
-        const chatID = req.body.chat;
-
-        contractModel(contract).save((err, createdContract) => {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                chatModel.findByIdAndUpdate(chatID, {$set: {"contract": createdContract._id.valueOf()}}).then(function (us, err) {
-                    if (err) {
-                        console.log(err);
-                        res.send(err);
-                    }
-                })
-            }
-        })
-    });
-
-    exports.updateContract = ((req, res) => {
-        const contract = req.body;
-        contractModel.findByIdAndUpdate(contract._id, {$set: contract}).then(function (updatedContract, err) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                res.send(updatedContract);
-            }
-        })
-    });
-
-    exports.updateContractDetails = ((req, res) => {
-        const contract = req.body;
-        contractModel.findByIdAndUpdate(contract._id, {
-            $set: {
-                "price": contract.price,
-                "startingDate": contract.startingDate
-            }
-        }).then(function (job, err) {
-            if (err) {
-                console.log(error);
-                res.send(error);
-            } else {
-                res.send(job);
-            }
-        })
-    });
-
-    exports.updateContractStatus = ((req, res) => {
-        const contract = req.body;
-        contractModel.findByIdAndUpdate(contract._id, {$set: {"paymentStatus": contract.paymentStatus}}).then(function (job, err) {
-            if (err) {
-                console.log(error);
-                res.send(error);
-            } else {
-                res.send(job);
-            }
-        })
-    });
-
-    exports.getContract = ((req, res) => {
-        contractModel.findById(req.query.contractId).then(function (contr, err) {
-            if (err) {
-                console.log(err);
-                res.send(err);
-            } else {
-                res.send(contr);
-            }
-        })
-    });
-
-    exports.getContractsFromIdArray = (async (req, res) => {
-        let contractsArray = [];
-        //console.log(req.body);
-        for (const contractId of req.query.idArray) {
-            await contractModel.findById(contractId).then((contr, err) => {
-                contractsArray.push(contr);
+exports.postMessageToChat = ((req, res) => {
+    let message = req.body;
+    messageModel(message).save((err, createdMessage) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            chatModel.findByIdAndUpdate(createdMessage.chat, {$push: {"messages": createdMessage._id.valueOf()}}).then(function (us, err) {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
             })
         }
-        res.send(contractsArray);
-    });
+    })
+});
 
-    exports.deleteContract = ((req, res) => {
-        contractModel.findByIdAndRemove(req.params.id).then(function (job, err) {
-            if (err) {
-                console.log(error);
-                res.send(error);
-            } else {
-                res.send("deleted");
-            }
+exports.deleteChat = ((req, res) => {
+    //TODO: maybe body, params, etc.
+    let chatIdToDelete = req.params.id;
+    console.log('chatIdToDelete');
+    console.log(chatIdToDelete)
+    //delete chat
+    chatModel.findByIdAndDelete(chatIdToDelete).then((deletedChat, err) => {
+        if (err) {
+            console.log('err in chat deletion');
+            console.log(err);
+            res.send(err);
+        } else {
+            //delete chat ids from users
+            ['client', 'craftsman'].forEach(clientOrCraftsman => {
+                userModel.findByIdAndUpdate(deletedChat.users[clientOrCraftsman], {$pull: {"chats": deletedChat._id}}).then((updatedUser, err) => {
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    }
+                })
+            })
+            //delete contract
+            contractModel.findByIdAndDelete(deletedChat.contract).then((deletedContract, err) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+            })
+            //delete messages
+            messageModel.deleteMany({chat: deletedChat._id}).then((deleted, err) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else console.log('deleted count: ' + deleted.deletedCount);
+            });
+
+            res.send(deletedChat);
+        }
+    })
+})
+
+//contracts
+exports.createContract = ((req, res) => {
+    const contract = req.body;
+    const chatID = req.body.chat;
+
+    contractModel(contract).save((err, createdContract) => {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            chatModel.findByIdAndUpdate(chatID, {$set: {"contract": createdContract._id.valueOf()}}).then(function (us, err) {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+            })
+        }
+    })
+});
+
+exports.updateContract = ((req, res) => {
+    const contract = req.body;
+    contractModel.findByIdAndUpdate(contract._id, {$set: contract}).then(function (updatedContract, err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.send(updatedContract);
+        }
+    })
+});
+
+exports.updateContractDetails = ((req, res) => {
+    const contract = req.body;
+    contractModel.findByIdAndUpdate(contract._id, {
+        $set: {
+            "price": contract.price,
+            "startingDate": contract.startingDate
+        }
+    }).then(function (job, err) {
+        if (err) {
+            console.log(error);
+            res.send(error);
+        } else {
+            res.send(job);
+        }
+    })
+});
+
+exports.updateContractStatus = ((req, res) => {
+    const contract = req.body;
+    contractModel.findByIdAndUpdate(contract._id, {$set: {"paymentStatus": contract.paymentStatus}}).then(function (job, err) {
+        if (err) {
+            console.log(error);
+            res.send(error);
+        } else {
+            res.send(job);
+        }
+    })
+});
+
+exports.getContract = ((req, res) => {
+    contractModel.findById(req.query.contractId).then(function (contr, err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            res.send(contr);
+        }
+    })
+});
+
+exports.getContractsFromIdArray = (async (req, res) => {
+    let contractsArray = [];
+    //console.log(req.body);
+    for (const contractId of req.query.idArray) {
+        await contractModel.findById(contractId).then((contr, err) => {
+            contractsArray.push(contr);
         })
-    });
+    }
+    res.send(contractsArray);
+});
+
+exports.deleteContract = ((req, res) => {
+    contractModel.findByIdAndRemove(req.params.id).then(function (job, err) {
+        if (err) {
+            console.log(error);
+            res.send(error);
+        } else {
+            res.send("deleted");
+        }
+    })
+});
 /*
 kept here for potential changes in the future
 
